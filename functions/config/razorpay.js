b3d3c5d204ab5e988a09adc761eb590b1e841588
Mevin-JR <supposedlysamyt@gmail.com>
2025-02-10 22:38:45 +0530

    Minor fixes, env deployment

--- functions/config/razorpay.js
+++ functions/config/razorpay.js
@@ -2,10 +2,10 @@ const { defineSecret } = require("firebase-functions/params");
 const Razorpay = require("razorpay");
 
 const keyId =
-    process.env.RAZORPAY_TEST_KEY || defineSecret("RAZORPAY_TEST_KEY");
+    process.env.RAZORPAY_TEST_KEY || defineSecret("RAZORPAY_TEST_KEY").value();
 const keySecret =
     process.env.RAZORPAY_TEST_KEY_SECRET ||
-    defineSecret("RAZORPAY_TEST_KEY_SECRET");
+    defineSecret("RAZORPAY_TEST_KEY_SECRET").value();
 
 const razorpay = new Razorpay({
     key_id: keyId,
