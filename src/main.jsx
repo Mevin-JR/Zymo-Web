bdcc2749fe6a0f4c6cb04ab49653c8bf984a0643
Kunj05 <kunjchandak05@gmail.com>
2025-03-26 20:15:37 +0530

    Removed the id

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-NKX8W30R7G"); // Replace with your Google Analytics Measurement ID
+ReactGA.initialize(""); // Replace with your Google Analytics Measurement ID
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
